df6dceeb63c0b89dcb530a4608bb6c887fbeee4a
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2025-01-29 23:23:08 +0200

    Created Basic Sidebard menu

--- Nexi.UI/ViewModels/MainViewModel.cs
+++ Nexi.UI/ViewModels/MainViewModel.cs
@@ -1,6 +1,94 @@
-﻿namespace Nexi.UI.ViewModels;
+﻿using ReactiveUI;
+using System.Windows.Input;
+
+namespace Nexi.UI.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    
+    private bool _isSidebarExpanded = true;
+    private double _sidebarWidth;
+    private ViewModelBase _currentPage;
+    private const double EXPANDED_WIDTH = 250;
+    private const double COLLAPSED_WIDTH = 60;
+
+    public MainViewModel()
+    {
+        UpdateSidebarWidth();
+
+        // Initialize commands
+        ToggleSidebarCommand = ReactiveCommand.Create(() =>
+        {
+            IsSidebarExpanded = !IsSidebarExpanded;
+        });
+
+        HomeCommand = ReactiveCommand.Create(NavigateToHome);
+        SettingsCommand = ReactiveCommand.Create(NavigateToSettings);
+        VoiceCommand = ReactiveCommand.Create(NavigateToVoice);
+        AIModelsCommand = ReactiveCommand.Create(NavigateToAIModels);
+        AboutCommand = ReactiveCommand.Create(NavigateToAbout);
+
+        // Set initial page
+        NavigateToHome();
+    }
+
+    public bool IsSidebarExpanded
+    {
+        get => _isSidebarExpanded;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _isSidebarExpanded, value);
+            UpdateSidebarWidth();
+        }
+    }
+
+    public double SidebarWidth
+    {
+        get => _sidebarWidth;
+        private set => this.RaiseAndSetIfChanged(ref _sidebarWidth, value);
+    }
+
+    public ViewModelBase CurrentPage
+    {
+        get => _currentPage;
+        private set => this.RaiseAndSetIfChanged(ref _currentPage, value);
+    }
+
+    private void UpdateSidebarWidth()
+    {
+        SidebarWidth = IsSidebarExpanded ? EXPANDED_WIDTH : COLLAPSED_WIDTH;
+    }
+
+    // Commands
+    public ICommand ToggleSidebarCommand { get; }
+    public ICommand HomeCommand { get; }
+    public ICommand SettingsCommand { get; }
+    public ICommand VoiceCommand { get; }
+    public ICommand AIModelsCommand { get; }
+    public ICommand AboutCommand { get; }
+
+    // Navigation Methods
+    private void NavigateToHome()
+    {
+        CurrentPage = new HomeViewModel();
+    }
+
+    private void NavigateToSettings()
+    {
+        CurrentPage = new SettingsViewModel();
+    }
+
+    private void NavigateToVoice()
+    {
+        CurrentPage = new VoiceCommandsViewModel();
+    }
+
+    private void NavigateToAIModels()
+    {
+        CurrentPage = new AIModelsViewModel();
+    }
+
+    private void NavigateToAbout()
+    {
+        CurrentPage = new AboutViewModel();
+    }
 }
