446d82a19e6bbce0fe241616cbfeeec8d3f1231d
Pavel Stoyanov <114023028+PavelStoyanov06@users.noreply.github.com>
2025-02-05 16:59:41 +0200

    Added Settings Menu

--- Nexi.UI/App.axaml.cs
+++ Nexi.UI/App.axaml.cs
@@ -1,36 +1,55 @@
 ﻿using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-
-using Nexi.UI.ViewModels;
+using Avalonia.Styling;
 using Nexi.UI.Views;
+using Nexi.UI.ViewModels;
+using Avalonia.Themes.Fluent;
 
-namespace Nexi.UI;
-
-public partial class App : Application
+namespace Nexi.UI
 {
-    public override void Initialize()
+    public partial class App : Application
     {
-        AvaloniaXamlLoader.Load(this);
-    }
-
-    public override void OnFrameworkInitializationCompleted()
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        public override void Initialize()
         {
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainViewModel()
-            };
-        }
-        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
-        {
-            singleViewPlatform.MainView = new MainView
-            {
-                DataContext = new MainViewModel()
-            };
+            AvaloniaXamlLoader.Load(this);
         }
 
-        base.OnFrameworkInitializationCompleted();
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow();
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+
+        public static void UpdateTheme(ThemeMode mode)
+        {
+            if (Current != null)
+            {
+                switch (mode)
+                {
+                    case ThemeMode.Light:
+                        Current.RequestedThemeVariant = ThemeVariant.Light;
+                        break;
+                    case ThemeMode.Dark:
+                        Current.RequestedThemeVariant = ThemeVariant.Dark;
+                        break;
+                    case ThemeMode.System:
+                        Current.RequestedThemeVariant = null;
+                        break;
+                }
+            }
+        }
+
+        public static void UpdateAccentColor(bool useSystem)
+        {
+            if (Current?.Styles[0] is FluentTheme fluentTheme)
+            {
+                // Placeholder for accent color logic
+            }
+        }
     }
 }
--- Nexi.UI/ViewModels/SettingsViewModel.cs
+++ Nexi.UI/ViewModels/SettingsViewModel.cs
@@ -1,12 +1,82 @@
-﻿using System;
+﻿using Avalonia;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Nexi.UI.ViewModels
 {
     public class SettingsViewModel : ViewModelBase
     {
+        private int _selectedModelIndex;
+        private bool _useGPU;
+        private int _selectedInputDeviceIndex;
+        private double _inputSensitivity = 50;
+        private ThemeMode _selectedTheme = ThemeMode.System;
+        private bool _useSystemAccent = true;
+
+        public SettingsViewModel()
+        {
+            // Subscribe to theme changes
+            this.WhenAnyValue(x => x.SelectedTheme)
+                .Subscribe(UpdateTheme);
+
+            this.WhenAnyValue(x => x.UseSystemAccent)
+                .Subscribe(UpdateAccentColor);
+        }
+
+        public IEnumerable<ThemeMode> ThemeOptions => Enum.GetValues<ThemeMode>();
+
+        public int SelectedModelIndex
+        {
+            get => _selectedModelIndex;
+            set => this.RaiseAndSetIfChanged(ref _selectedModelIndex, value);
+        }
+
+        public bool UseGPU
+        {
+            get => _useGPU;
+            set => this.RaiseAndSetIfChanged(ref _useGPU, value);
+        }
+
+        public int SelectedInputDeviceIndex
+        {
+            get => _selectedInputDeviceIndex;
+            set => this.RaiseAndSetIfChanged(ref _selectedInputDeviceIndex, value);
+        }
+
+        public double InputSensitivity
+        {
+            get => _inputSensitivity;
+            set => this.RaiseAndSetIfChanged(ref _inputSensitivity, value);
+        }
+
+        public ThemeMode SelectedTheme
+        {
+            get => _selectedTheme;
+            set => this.RaiseAndSetIfChanged(ref _selectedTheme, value);
+        }
+
+        public bool UseSystemAccent
+        {
+            get => _useSystemAccent;
+            set => this.RaiseAndSetIfChanged(ref _useSystemAccent, value);
+        }
+
+        private void UpdateTheme(ThemeMode mode)
+        {
+            App.UpdateTheme(mode);
+        }
+
+        private void UpdateAccentColor(bool useSystem)
+        {
+            App.UpdateAccentColor(useSystem);
+        }
+    }
+
+    public enum ThemeMode
+    {
+        System,
+        Light,
+        Dark
     }
 }
